b83d1bb8e054acdc7bc5781afd14a0d514ff3f9c
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2025-04-15 19:31:59 +0100

    tests, IDP analytical eqns latex

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gofai-vs-learnt-control",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "gymnasium",
+        "numpy",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+) 
